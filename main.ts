2d9a5061cc354561b03f2542a2edb35ee654dcbd
caiodna360 <caio.nogueira@dna360.ag>
2023-08-23 16:17:50 -0300

    feat: commit dia 23/08 correções

--- main.ts
+++ main.ts
@@ -3,14 +3,17 @@
 /// <reference lib="deno.ns" />
 /// <reference lib="esnext" />
 
+
 import { start } from "$fresh/server.ts";
 import { $live } from "$live/mod.ts";
 import partytownPlugin from "partytown/mod.ts";
 import manifest from "./live.gen.ts";
 import site from "./site.json" assert { type: "json" };
+import plugins from 'deco-sites/std/plugins/mod.ts';
 
 await start($live(manifest, site), {
   plugins: [
+    ...plugins(),
     partytownPlugin(),
   ],
 });
--- sections/Content/Lojas.tsx
+++ sections/Content/Lojas.tsx
@@ -1,35 +1,36 @@
 export interface Loja {
-    title?: string;
-    phone?: string;
-    email?: string;
-    adress?: {
-        firstLine?: string;
-        secondLine?: string
-    }
-  }
+  title?: string;
+  phone?: string;
+  email?: string;
+  adress?: {
+    firstLine?: string;
+    secondLine?: string;
+  };
+}
 export interface Props {
-    title: string;
-    lojas: Array<Loja>;
+  title: string;
+  lojas: Array<Loja>;
 }
 
-export default function Lojas({ title,lojas }: Props) {
-    return (
-        <section>
-            <h2>{title}</h2>
-            <ul>
-                {lojas.map((loja) => {
-                    return (
-                    <li>
-                        <div>
-                            <h3>{ loja.title }</h3>
-                            <p>{ loja.phone }</p>
-                            <p>{ loja.email }</p>
-                            <p>{ loja.adress?.firstLine }</p>
-                            <p>{ loja.adress?.secondLine }</p>
-                        </div>
-                    </li> )
-                }) }
-            </ul>
-        </section>
-    )
+export default function Lojas({ title, lojas }: Props) {
+  return (
+    <section>
+      <h2 >{title}</h2>
+      <ul>
+        {lojas.map((loja) => {
+          return (
+              <li>
+                <div>
+                  <h3>{loja.title}</h3>
+                  <p>{loja.phone}</p>
+                  <p>{loja.email}</p>
+                  <p>{loja.adress?.firstLine}</p>
+                  <p>{loja.adress?.secondLine}</p>
+                </div>
+              </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
 }
