9981eb4f4bce38b806d66b88a438d1581e46bb35
gimenes <tlgimenes@gmail.com>
2023-08-23 17:10:33 -0300

    fix css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,5 @@
 import daisyui from "daisyui";
 
-console.log({
-  plugins: [daisyui],
-  daisyui: { themes: [], logs: false },
-  content: ["./**/*.tsx"],
-  theme: { container: { center: true } },
-});
-
 export default {
   plugins: [daisyui],
   daisyui: { themes: [], logs: false },
